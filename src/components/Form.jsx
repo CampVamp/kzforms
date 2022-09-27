6921da1d2f622104bedb5dd6a959c92cb42c9c11
CampVamp <ajayramsaravanan4@gmail.com>
2022-09-27 22:44:08 +0530

    Added Submited Form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,40 +1,56 @@
 import axios from "axios";
 import TextField from "./TextField";
+import SubmitForm from "./SubmitForm";
+import { useState } from "react";
 
 const Form = () => {
+  const [data, setData] = useState({});
+  const [showform, setShowForm] = useState(true);
+  let formdata;
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log(event.target.name.value);
+    formdata = {
+      name: event.target.name.value,
+      regNo: event.target.regNo.value,
+      pEmail: event.target.pEmail.value,
+      sEmail: event.target.sEmail.value,
+      gitlink: event.target.gitlink.value,
+    };
     axios
-      .post(`${process.env.REACT_APP_API_BASE_URL}/posts`, {
-        name: event.target.name.value,
-        regNo: event.target.regNo.value,
-        pEmail: event.target.pEmail.value,
-        sEmail: event.target.sEmail.value,
-        gitlink: event.target.gitlink.value,
+      .post(`${process.env.REACT_APP_API_BASE_URL}/posts`, formdata)
+      .then((res) => {
+        console.log(res);
+        setData(formdata);
+        setShowForm(false);
       })
-      .then((res) => console.log(res))
       .catch((err) => console.error(err));
   };
 
+  console.log("AJay", data);
+
   return (
     <div className="Main-Frame">
-      <h1 className="Form-Title">REGISTRATION</h1>
-      <form onSubmit={handleSubmit}>
-        <TextField text="Name" name="name" />
-        <br />
-        <TextField text="Registration No" name="regNo" />
-        <br />
-        <TextField text="Personal Email" name="pEmail" />
-        <br />
-        <TextField text="SRMIST Email" name="sEmail" />
-        <br />
-        <TextField text="Github Link" name="gitlink" />
-        <br />
-        <button className="Form-Btn" type="Submit">
-          SUBMIT
-        </button>
-      </form>
+      {showform && (
+        <>
+          <h1 className="Form-Title">REGISTRATION</h1>
+          <form onSubmit={handleSubmit}>
+            <TextField text="Name" name="name" />
+            <br />
+            <TextField text="Registration No" name="regNo" />
+            <br />
+            <TextField text="Personal Email" name="pEmail" />
+            <br />
+            <TextField text="SRMIST Email" name="sEmail" />
+            <br />
+            <TextField text="Github Link" name="gitlink" />
+            <br />
+            <button className="Form-Btn" type="Submit">
+              SUBMIT
+            </button>
+          </form>
+        </>
+      )}
+      {!showform && <SubmitForm />}
     </div>
   );
 };
---
+++ src/components/SubmitForm.jsx
@@ -0,0 +1,9 @@
+const SubmitForm = () => {
+  return (
+    <div>
+      <h1>Thank you for filling the Form</h1>
+    </div>
+  );
+};
+
+export default SubmitForm;
